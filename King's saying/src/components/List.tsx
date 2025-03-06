e1e33673fc2d12930fc172163380cba3b30e0401
bbibbaroni <i89155345@gmail.com>
2025-03-06 23:52:54 +0900

    [King's Saying]

---
+++ King's saying/src/components/List.tsx
@@ -0,0 +1,215 @@
+import "./List.css";
+
+const List = () => {
+  return (
+    <>
+      <header>
+        <div className="title_page">
+          <a href="#home" className="castoro-regular">
+            KING'S SAYING
+          </a>
+        </div>
+      </header>
+
+      <main>
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="scripts">
+          <h2>왕의 전언들</h2>
+          <h1>"로마법에 가서 로마를 따르라"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 13일</p>
+          <br />
+          <h1>"내가 심부름센터 직원이냐"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 18일</p>
+          <br />
+          <h1>"두 사람이 빗자루로 칼싸움을 합니다.. 우와와@악"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <br />
+          <h1>"휠+체어를 타다."</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <br />
+          <h1>"정관장 홍삼 리미티드..그..빨아먹는 거"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <br />
+          <h1>
+            "청소를 한놈은 정상입니다, 안한 놈도 정상입니다."
+            <br />
+            "잘못된 거 아닙니까?"
+          </h1>
+          <p className="centerAlign TopAlign">2024년 3월 18일</p>
+          <br />
+          <h1>"이름표에 사진 안 단 놈들은 투명인간입니까?"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 18일</p>
+          <br />
+          <h1>"내 별명은 마당쇠입니다. 머슴이라는 뜻입니다."</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <br />
+          <h1>"나는 FM대로 합니다. 요령 것하십시오."</h1>
+          <p className="centerAlign TopAlign">2024년 3월 18일</p>
+          <br />
+          <h1>"우스갯소리로.."</h1>
+          <p className="centerAlign TopAlign">always</p>
+          <br />
+          <h1>"어어? 거기 웃지? 재밌나보죠?"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <br />
+          <h1>"침 뱉고 미때면서 확인을 합니다."</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <br />
+          <h1>"제가 영화배우를 했습니다. 쪽팔립니다."</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <br />
+          <h1>"(주섬주섬) 봐 여기 찍혀있지?"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <br />
+          <h1>
+            "저번에 키득키득 웃거군요 그 칼싸움..야구"
+            <br />
+            "청소 끝나고 하십시오."
+          </h1>
+          <p className="centerAlign TopAlign">2024년 3월 19일</p>
+          <br />
+          <h1>"장담컨데 우스갯소리로 저는 여러분들 부모입니다."</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <br />
+          <h1>
+            "저는 끝나고 타임방에서 모니터링을..아니 모니터링실에서 보고
+            있습니다."
+          </h1>
+          <p className="centerAlign TopAlign">2024년 3월 19일</p>
+          <br />
+          <h1>
+            "점호 끝나고 카톡이 오면 제가 무안합니까? 안 무안합니까?"
+            <br />
+            "안 무안.. 무안합니다."
+          </h1>
+          <p className="centerAlign TopAlign">2024년 3월 19일</p>
+          <br />
+          <h1>"(딸깍딸깍)"</h1>
+          <p className="centerAlign TopAlign">always</p>
+          <br />
+          <h1>"이 숙소가 무궁화 세개 달린 호텔이 아니지않습니까?"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 20일</p>
+          <br />
+          <h1>"청소를 하지 않으면 불미스러운 일을 당할 수 있습니다."</h1>
+          <p className="centerAlign TopAlign">2024년 3월 20일</p>
+          <br />
+          <h1>"인사를 왜 이렇게 크게합니까? 지금 뭐 데모합니까?"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <br />
+          <h1>"계속 저러는건 장에 문제가 있는겁니까? 정상인겁니까?"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 21일</p>
+          <h1>"부모의 마음으로 해줬더니 종처럼 취급합니다."</h1>
+          <p className="centerAlign TopAlign">2024년 3월 25일</p>
+          <br />
+          <h1>"그렇지 않습니까?"</h1>
+          <p className="centerAlign TopAlign">always</p>
+          <br />
+          <h1>"빠르면 40분 늦어도 40분 입니다"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 31일</p>
+          <br />
+          <h1>"마 일로와봐 (네?) 니 청장해라. 지금부터 자기소개 시작"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 n일</p>
+          <p className="Ps_or_Sub_Description">ps. 강제청장시작</p>
+          <h1>
+            "예전에 돈좀 벌어 보겠다고 몇억을 들여서 가게를 차렸습니다"
+            <br />
+            "정말 엄청난일이 막 벌어지고, 불미스러운 일이 정말 많이 생깁니다"
+          </h1>
+          <p className="centerAlign TopAlign">2024년 4월 2일</p>
+          <p className="Ps_or_Sub_Description">
+            ps. 결말이 없는 말에 모든 당시 학생들이 당황했다
+          </p>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          {/* 0326 마당정변 해당 일시의 문건은 1급 사항으로 귀중한 자료로 취급됨. */}
+          <br />
+          <br />
+          <br />
+          <br />
+          <h1 className="MADANG_JEONGBYEON">
+            {" "}
+            <p className="Sub_Title"> -Imortant Document- </p> <br /> ༺ MADANG
+            REVOLUTOIN ༻ <br />
+            <br /> <p className="Sub_Title"> &lt; 마당혁명 &gt;</p>{" "}
+          </h1>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <h1 className="madangbird0326 centerAlign BottomAlign">
+            주의
+            <br />
+            3월 26일의 마당혁명이 9:00부터 9:15까지 이어진 광란의 혁명이다.
+            <br />
+            아래의 문건은 모두 혁명의 주동자인 "마당쇠"의 어록들이다
+            <br />
+            해당 혁명의 피해자일당의 가장 선두에 있던 필자가 귀중히 밀입한
+            내용이니
+            <br />
+            경건하고 엄숙한 마음가짐으로 볼 것을 권한다.
+          </h1>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <h1>"왜 여기는 중앙청소구역이 아니야? 38선인가 보네"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 26일</p>
+          <br />
+          <h1>"민재, 내 대학교가 어딘지 궁금해 (네) 쪽팔려서 말 못합니다"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 26일</p>
+          <br />
+          <h1>"어이 317호 어제 여기서 뒹굴던데 바닥 청소하나? 주짓수 하나?"</h1>
+          <p className="centerAlign TopAlign">
+            2024년 3월 26일
+            <br />
+          </p>
+          <p className="Ps_or_Sub_Description">
+            ps. 당일 317호 필자는 주짓수가 아니라 더러운 바닥에서 구른다고,
+            대단하다는 칭호를 획득했다.
+          </p>
+          <p />
+          <br />
+          <h1>"웃어? 무시하나 보네? 내가 동물원의 원숭이야?"</h1>
+          <p className="centerAlign TopAlign">2024년 3월 26일</p>
+          <br />
+          <h1>
+            "지금 저 밑에 말하는 사람, 아는사람 있습니까?"
+            <br />
+            "나중에 가서 말해보십쇼 (넵) 니 그거 진실이가 ? 전 1%도 신뢰하지
+            않습니다"
+          </h1>
+          <p className="centerAlign TopAlign">2024년 3월 26일</p>
+          <br />
+          <h1>
+            "내가 저번에 여기서 바퀴 시늉(휠+체어)까지 하면서 보여줬지
+            않습니까?"
+          </h1>
+          <p className="centerAlign TopAlign">2024년 3월 26일</p>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default List;
